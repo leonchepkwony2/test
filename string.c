03c35a084881000a581899eeb163bdaf49d28d62
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-04-26 02:46:45 -0700

    ad

---
+++ string.c
@@ -0,0 +1,18 @@
+#include "myShell.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @str: the string to be measured
+ *
+ * Return: the length of the string
+ */
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+	
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
