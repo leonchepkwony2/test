bd34743925da439acee4e17078504ef5a82a803f
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-04-28 05:33:44 -0700

    string.c

--- display.c
+++ display.c
@@ -1,57 +1,24 @@
-#include "shell.h"
-
 /**
- * _fgets - reads a line from a file stream and stores it into a string
- * @str: the buffer to store the read line
- * @num: the maximum number of characters to be read
- * @stream: the file stream to read from
+ * my_strcspn - get length of prefix
+ * substring that doesn't contain specified characters
+ * @s: string to search
+ * @reject: string containing characters to exclude
  *
- * Return: On success, the pointer to the buffer containing the read line.
- *         If end-of-file occurs before any characters are read, the function
- *         returns NULL and the buffer contents remain unchanged.
+ * Return: number of characters in s that do not occur in reject
  */
-char *_fgets(char *str, int num, FILE *stream)
+size_t _strcspn(const char *s, const char *reject)
 {
-	char *newline_pos;
-	char *result = fgets(str, num, stream);
+	const char *p, *q;
 	
-	if (result == NULL)
-		return (NULL);
-
-	 newline_pos = _strchr(str, '\n');
-
-	if (newline_pos != NULL)
+	for (p = s; *p; p++)
 	{
-		*newline_pos = '\0';
-	}
-	else
-	{
-		int c;
-
-		while ((c = _getchar()) != '\n' && c != EOF)
+		for (q = reject; *q; q++)
 		{
+			if (*p == *q)
+			{
+				return (p - s);
+			}
 		}
 	}
-
-	return (str);
-}
-
-
-/**
- * _feof - checks if end-of-file indicator is set for a stream
- *
- * @stream: pointer to the stream to check
- *
- * Return: 1 if end-of-file indicator is set, 0 otherwise
- */
-int _feof(FILE *stream)
-{
-	long pos = ftell(stream);
-	int c = getc(stream);
-	
-	fseek(stream, pos, SEEK_SET);
-	if (c == EOF && ferror(stream))
-		return (1);
-	else
-		return (0);
+	return (p - s);
 }
