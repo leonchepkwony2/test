926bf225232bbfc3f172dc3c6ece389524f214dd
LEONARD CHEPKWONY <leonchepkwony2@gmail.com>
2023-04-28 05:28:18 -0700

    utility.c

--- display.c
+++ display.c
@@ -1,54 +1,57 @@
 #include "shell.h"
 
-extern char **environ;
+/**
+ * _fgets - reads a line from a file stream and stores it into a string
+ * @str: the buffer to store the read line
+ * @num: the maximum number of characters to be read
+ * @stream: the file stream to read from
+ *
+ * Return: On success, the pointer to the buffer containing the read line.
+ *         If end-of-file occurs before any characters are read, the function
+ *         returns NULL and the buffer contents remain unchanged.
+ */
+char *_fgets(char *str, int num, FILE *stream)
+{
+	char *newline_pos;
+	char *result = fgets(str, num, stream);
+	
+	if (result == NULL)
+		return (NULL);
+
+	 newline_pos = _strchr(str, '\n');
+
+	if (newline_pos != NULL)
+	{
+		*newline_pos = '\0';
+	}
+	else
+	{
+		int c;
+
+		while ((c = _getchar()) != '\n' && c != EOF)
+		{
+		}
+	}
+
+	return (str);
+}
 
 
 /**
- * main - Entry point for the shell program.
+ * _feof - checks if end-of-file indicator is set for a stream
  *
- * Return: 0 on success
+ * @stream: pointer to the stream to check
+ *
+ * Return: 1 if end-of-file indicator is set, 0 otherwise
  */
-int main(void)
+int _feof(FILE *stream)
 {
-	char cmd[MAX_CMD_LENGTH];
-	char *argv[] = {NULL, NULL};
-	int status;
+	long pos = ftell(stream);
+	int c = getc(stream);
 	
-	while (1)
-	{
-		_putchar(PROMPT);
-		fflush(stdout);
-		if (_fgets(cmd, MAX_CMD_LENGTH, stdin) == NULL)
-		{
-			if (_feof(stdin))
-			{
-				_putchar('\n');
-				exit(EXIT_SUCCESS);
-			}
-			perror("fgets");
-			exit(EXIT_FAILURE);
-		}
-		cmd[_strcspn(cmd, "\n")] = '\0';
-		argv[0] = cmd;
-		pid_t pid = fork();
-		
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			if (execve(cmd, argv, environ) == -1)
-			{
-				perror("execve");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-		}
-	}
-	return (0);
+	fseek(stream, pos, SEEK_SET);
+	if (c == EOF && ferror(stream))
+		return (1);
+	else
+		return (0);
 }
